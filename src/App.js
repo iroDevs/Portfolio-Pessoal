f65b9ebcef0d64497d5ecc40a9169c3553a96629
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-17 17:59:03 -0300

    criando componentes iniciais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
+import ContainerAbove from './containers/Container-above/ContainerAbove'
 
 function App () {
   return (
     <div className="App">
-
+      <div className="container-main">
+      <ContainerAbove />
+      </div>
     </div>
   )
 }
