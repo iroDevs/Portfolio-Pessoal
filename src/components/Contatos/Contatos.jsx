2d534377b37d658a7135791fa9f057e103ef9a12
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-19 00:21:38 -0300

    concertando links da lista

--- src/components/Contatos/Contatos.jsx
+++ src/components/Contatos/Contatos.jsx
@@ -14,9 +14,9 @@ export default function Contatos () {
     <div className="list-contatos">
         <li onClick={copiaTexto}>Pedro Henrique Loures Oliveira</li>
         <li onClick={copiaTexto}><AiOutlineMail />pedrohenriquelouresoliveira@gmail.com</li>
-        <li><AiOutlineWhatsApp /><a target="_blank" href="https://wa.me/5532999359393" rel="noreferrer">(32) 9 99359393</a></li>
-        <li><AiFillLinkedin /><a target="_blank" href="https://www.linkedin.com/in/pedro-2001/" rel="noreferrer">Linkedin</a> </li>
-        <li><AiFillGithub /><a target="_blank" href="https://github.com/iroDevs" rel="noreferrer">gitHub</a> </li>
+        <a target="_blank" href="https://wa.me/5532999359393" rel="noreferrer"><li><AiOutlineWhatsApp />(32) 9 99359393</li></a>
+        <a target="_blank" href="https://www.linkedin.com/in/pedro-2001/" rel="noreferrer"> <li><AiFillLinkedin />Linkedin </li></a>
+        <a target="_blank" href="https://github.com/iroDevs" rel="noreferrer"> <li><AiFillGithub />gitHub </li></a>
     </div>
 
     </>
