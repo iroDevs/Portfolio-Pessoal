fab8c24678bf493ac7a62a105e839c51c7148412
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-19 00:49:17 -0300

    criando skills

--- src/containers/Container-above/ContainerAbove.jsx
+++ src/containers/Container-above/ContainerAbove.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Foto from '../../components/Foto/Foto'
 import Contatos from '../../components/Contatos/Contatos'
+import ListaTec from '../../components/ListaTec/ListaTec'
 import './ContainerAboveStyle.css'
 export default function ContainerAbove () {
   return (
@@ -8,6 +9,9 @@ export default function ContainerAbove () {
         <Foto />
         <div className=" container-info">
         <Contatos />
+        <div className="container container-profile">
+        <ListaTec />
+        </div>
         </div>
     </div>
   )
