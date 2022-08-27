c156a79f43826c33aba9a48b26843364d2b3a74e
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-26 22:20:46 -0300

    experincia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
 import ContainerAbove from './containers/Container-above/ContainerAbove'
+import ContainerExp from './containers/Container-exp/ContainerExp'
 
 function App () {
   return (
     <div className="App">
       <div className="container-fluid container-main">
       <ContainerAbove />
+      <ContainerExp />
       </div>
+
     </div>
   )
 }
