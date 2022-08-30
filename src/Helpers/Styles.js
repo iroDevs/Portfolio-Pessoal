de3f069ef8086a398d7531acc9e114ac03ac74e7
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-30 14:33:32 -0300

    style

---
+++ src/Helpers/Styles.js
@@ -0,0 +1,22 @@
+const claro =
+{
+  backgroundColor: '#f5b351',
+  color: 'black'
+}
+
+const escuro =
+{
+  backgroundColor: '#18191a',
+  color: '#ffffff'
+}
+
+const colorido =
+{
+  background: 'linear-gradient(45deg,rgb(0, 0, 0),rgb(18, 5, 138))',
+  backgroundSize: '700% 700%',
+  animation: 'colors 6s ease infinite'
+}
+
+const estilos = [escuro, claro, colorido]
+
+export default estilos
