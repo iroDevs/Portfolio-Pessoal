040594836bcb6bbbd4c0a6720f54364f414a1ae2
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-08-30 14:32:41 -0300

    selecionar modo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-unused-vars */
+
 import './App.css'
 import ContainerAbove from './containers/Container-above/ContainerAbove'
 import ContainerExp from './containers/Container-exp/ContainerExp'
 import ZapButton from './components/ZapButton/ZapButton'
 import StyleButton from './components/outros/StyleButton'
+import Context from './context/Context'
+import Estilos from './Helpers/Styles'
+import React, { useContext, useState } from 'react'
 
 function App () {
+  const { modo } = useContext(Context)
+  console.log(Estilos[modo])
   return (
-    <div className="App">
+    <div className="App" style={Estilos[modo]}>
       <div className="container-fluid container-main">
       <ContainerAbove />
       <ContainerExp />
